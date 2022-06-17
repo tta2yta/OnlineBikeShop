2043b0726513e0080675ae6280d123ea222e21d6
tta2yta <tta2yta@gmail.com>
2022-06-18 00:07:44 +0300

    add Model class

---
+++ Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineBikeShop.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Make Make { get; set; }
+
+        [ForeignKey("MakeId")]
+        public int MakeId { get; set; }
+    }
+}
