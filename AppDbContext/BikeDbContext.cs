c5528fe665962a1192d66a11e53a7f073a5d0126
tta2yta <tta2yta@gmail.com>
2022-06-18 15:57:03 +0300

    add BikeDbcontext

---
+++ AppDbContext/BikeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBikeShop.Models;
+
+namespace OnlineBikeShop.AppDbContext
+{
+    public class BikeDbContext: DbContext
+    {
+        public BikeDbContext( DbContextOptions<BikeDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Make> Makes { get; set; }
+        public DbSet <Model> Models { get; set; }
+    }
+}
