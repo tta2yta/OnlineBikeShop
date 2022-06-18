19f0af6814ca879072c01fc60434be065ff620d4
tta2yta <tta2yta@gmail.com>
2022-06-18 16:11:14 +0300

    add dependecy injection for BikeDbContext in program.cs and add connection string

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineBikeShop.AppDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//add dbcontext
+builder.Services.AddDbContext<BikeDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
