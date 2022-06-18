526473e251dc1cf90d0146edcdfff000617af078
tta2yta <tta2yta@gmail.com>
2022-06-18 18:25:32 +0300

    add edit-delete options to make view

--- Controllers/MakeController.cs
+++ Controllers/MakeController.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBikeShop.AppDbContext;
 using OnlineBikeShop.Models;
 
 namespace OnlineBikeShop.Controllers
 {
     public class MakeController : Controller
     {
+        private readonly BikeDbContext _db;
+
+        public MakeController(BikeDbContext db)
+        {
+                _db= db;
+        }
         public IActionResult Bikes()
         {
             Make make = new Make { Id = 1, Name = "Ted" };
             return View(make);
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Make> makes = _db.Makes;
+            return View(makes);
         }
 
         [Route("make/bikes/{month:int:length(4)}/{years:int:range(1,10)}")]
